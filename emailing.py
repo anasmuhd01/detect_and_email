c60848ed58751d648ed71a68532f81d56f62f259
anas <ansmuhd01@gmail.com>
2023-08-31 11:47:39 +0530

    send_mail()

---
+++ emailing.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+
+def send_mail():
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "beerankutty1509@gmail.com"
+    password = "sbbvdfarbygnwcwg"
+
+    receiver = "beerankutty1509@gmail.com"
+
+    context = ssl.create_default_context()
+
+    message = """
+    """
+
+    with smtplib.SMTP_SSL(host , port, context=context)as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
