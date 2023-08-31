4fc8c4c1abd46dac1a6d80a6c0a5ef49b73bc8b5
anas <ansmuhd01@gmail.com>
2023-08-31 11:02:41 +0530

    grey frame, gausian blur added

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ time.sleep(1)
 
 while True:
     check, frame = video.read()
-    cv2.imshow("my video", frame)
+    # grey_frame : to reduce matrix complexity color changed to grey
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gausian blur : to make calculations more efficient
+    grey_frame_gaus = cv2.GaussianBlur(grey_frame, (21, 21), 0)
+
+    cv2.imshow("my video", grey_frame_gaus)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
